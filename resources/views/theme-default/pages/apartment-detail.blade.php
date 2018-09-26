c38e53fcaad327cde4ab5a77d18f566c172cd51b
Thien Pham <chithien175@gmail.com>
2018-09-26 14:43:42 +0700

    update

--- resources/views/theme-default/pages/apartment-detail.blade.php
+++ resources/views/theme-default/pages/apartment-detail.blade.php
@@ -77,24 +77,7 @@
             <!--End  single_tour_desc-->
 
             <div class="col-md-4">
-                <div class="box_style_3">
-                    <i class="icon_set_1_icon-57"></i>
-                    <h4><span>Đặt phòng</span> ngay!</h4>
-                    <h4>
-                        <a href="{{ setting('lien-he.phone') }}" class="phone">{{ setting('lien-he.phone') }}</a>
-                    </h4>
-                </div>
-                <div class="widget" id="cat_blog">
-                    <h4>Danh mục nổi bật</h4>
-                    <ul>
-                        <li><a href="{{ route('frontend.pages.tour') }}"><i class="icon_set_2_icon-108"></i> Tour</a>
-                        </li>
-                        <li><a href="{{ route('frontend.pages.apartment') }}"><i class="icon_set_1_icon-23"></i> Căn hộ</a>
-                        </li>
-                        <li><a href="{{ route('frontend.pages.car') }}"><i class=" icon_set_1_icon-26"></i> Dịch vụ xe đưa đón</a>
-                        </li>
-                    </ul>
-                </div>
+                @include('theme-default.partials.sidebar', ['type' => 'apartment'])
             </div>
             <!-- End col-md-4 -->
         </div>
